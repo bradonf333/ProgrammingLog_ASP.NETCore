34e2465cd1deebff682612edcbe673e0d57c5df2
bradonf333 <bradonf123@gmail.com>
2017-10-14 10:41:32 -0600

    Installed NPM and added new component

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppModuleShared } from './app.module.shared';
 import { AppComponent } from './components/app/app.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
         ServerModule,
         AppModuleShared
-    ]
+    ],
+    declarations: [TaskFormComponent]
 })
 export class AppModule {
 }
