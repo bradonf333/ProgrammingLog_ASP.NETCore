63835b17b7097bfdb1715b979cb9b64af1a5d8f7
bradonf333 <bradonf123@gmail.com>
2017-10-14 18:50:30 -0600

    SeedTaskLanguages migration

---
+++ Migrations/20171015004825_SeedTaskLanguages.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ProgrammingLog.Migrations
+{
+    public partial class SeedTaskLanguages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (1, 2)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (2, 2)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (3, 2)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (4, 1)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (5, 2)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (6, 2)");
+            migrationBuilder.Sql("INSERT INTO TaskLanguages (TaskId, LanguageId) VALUES (7, 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM TaskLanguages WHERE TaskId IN (1, 2, 3, 4, 5, 6, 7)");
+        }
+    }
+}
