130c6d6ce5764d717d7d0d8bade9dae00a269cbb
bradonf333 <bradonf123@gmail.com>
2017-11-15 18:03:03 -0700

    Get the task id from the route

--- ClientApp/app/components/task-form/task-edit.component.ts
+++ ClientApp/app/components/task-form/task-edit.component.ts
@@ -1,3 +1,6 @@
+import { TaskService } from './../../service/task.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProgrammingTask } from './../app/models/task';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-edit.component.css']
 })
 export class TaskEditComponent implements OnInit {
+  pageTitle: string = "Edit a Programming Task";
+  languages: any[];
+  task: ProgrammingTask = {
+    id: 0,
+    hours: '',
+    description: '',
+    summary: '',
+    taskDate: '',
+    programmingLanguages: []
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private taskService: TaskService) {
+    route.params.subscribe(t => {
+      this.task.id = + t['id'];
+    })
+  }
 
   ngOnInit() {
   }
