54206e92345f212ba6ed6e29c515163c0078a247
bradonf333 <bradonf123@gmail.com>
2018-01-08 15:16:34 -0700

    Working on Extensable IQueryable method

--- ClientApp/app/components/task-list/task-list.component.ts
+++ ClientApp/app/components/task-list/task-list.component.ts
@@ -13,6 +13,7 @@ export class TaskListComponent implements OnInit {
 
   pageTitle: string = "Tasks";
   allTasks: SaveProgrammingTask[];
+  // filteredTasks: SaveProgrammingTask[] = [];
   filter: any = {};
   sort: string;
   reverseSort: boolean = false;
@@ -22,7 +23,10 @@ export class TaskListComponent implements OnInit {
 
   ngOnInit() {
 
-    this.populateTasks();
+    this.taskService.getTasks(this.filter)
+      .subscribe(tasks => {
+        this.allTasks = tasks;
+      });
 
     this.taskService.getLanguages()
       .subscribe(languages => {
@@ -30,21 +34,171 @@ export class TaskListComponent implements OnInit {
       });
   }
 
-  private populateTasks() {
-    this.taskService.getTasks(this.filter)
-      .subscribe(tasks => this.allTasks = tasks);
-  }
+  /**
+   * Takes a string parameter and sorts the lists of tasks by the given string.
+   * @param sortBy 
+   */
+  // onSort(sortBy: string) {
 
+  //   this.sort = sortBy;
+  //   var tasks = this.filteredTasks;
+
+  //   // Seems like there may be a better way to do this!
+
+  //   if (this.sort === "taskId") {
+  //     tasks = tasks.sort((task1: SaveProgrammingTask, task2: SaveProgrammingTask) => {
+  //       if (task1.id < task2.id) {
+  //         return -1;
+  //       } else if (task1.id > task2.id) {
+  //         return 1
+  //       } else {
+  //         return 0
+  //       }
+  //     });
+  //   }
+
+  //   if (this.sort === "taskHour") {
+  //     tasks = tasks.sort((task1: SaveProgrammingTask, task2: SaveProgrammingTask) => {
+  //       if (task1.hours < task2.hours) {
+  //         return -1;
+  //       } else if (task1.hours > task2.hours) {
+  //         return 1
+  //       } else {
+  //         return 0
+  //       }
+  //     });
+  //   }
+
+  //   if (this.sort === "taskLanguage") {
+  //     tasks = tasks.sort((task1: SaveProgrammingTask, task2: SaveProgrammingTask) => {
+  //       if (task1.languages < task2.languages) {
+  //         return -1;
+  //       } else if (task1.languages > task2.languages) {
+  //         return 1
+  //       } else {
+  //         return 0
+  //       }
+  //     });
+  //   }
+
+  //   if (this.sort === "taskSummary") {
+  //     tasks = tasks.sort((task1: SaveProgrammingTask, task2: SaveProgrammingTask) => {
+  //       if (task1.summary < task2.summary) {
+  //         return -1;
+  //       } else if (task1.summary > task2.summary) {
+  //         return 1
+  //       } else {
+  //         return 0
+  //       }
+  //     });
+  //   }
+
+  //   if (this.sort === "taskDate") {
+  //     tasks = tasks.sort((task1: SaveProgrammingTask, task2: SaveProgrammingTask) => {
+  //       if (task1.taskDate < task2.taskDate) {
+  //         return -1;
+  //       } else if (task1.taskDate > task2.taskDate) {
+  //         return 1
+  //       } else {
+  //         return 0
+  //       }
+  //     });
+  //   }
+
+  //   if (!this.reverseSort) {
+  //     this.filteredTasks = tasks;
+  //   } else {
+  //     this.filteredTasks = tasks.reverse();
+  //   }
+
+  //   this.reverseSort = !this.reverseSort;
+  // }
 
   onLanguageFilterChange() {
-    this.populateTasks();
-  }
 
-  onFilterChange() {
-    this.populateTasks();
-  }
+    var tasks = this.allTasks;
 
-  onTaskSummaryFilter() {
-    this.populateTasks();
+    /**
+     * If the TaskSummaryKeyWord Filter has a value we need to call that filter function to set the filtered list value
+     * based on that filter first.
+     * If it has no value then we just reset the filtered tasks to all tasks
+     */
+    // if (this.filter.summaryKeyWord) {
+    //   this.onTaskSummaryFilter();
+    //   tasks = this.filteredTasks;
+    // }
+    // else {
+    //   tasks = this.allTasks;
+    // }
+
+    // if (this.filter.languageId) {
+    //   // Filter & some version of my nested for loops below!
+    //   this.filteredTasks = tasks.filter(
+    //     (task) => task.languages.some(
+    //       (language) => language.id == this.filter.languageId));
+
+      // This is a nested for loop version.... Much more work. Kinda keeping just so I can realize how much better the filter and some functions are
+
+      // Reset filteredTasks
+      // this.filteredTasks = [];
+
+      // for (let index = 0; index < tasks.length; index++) {
+      //   const task = tasks[index];
+
+      //   for (let index2 = 0; index2 < task.languages.length; index2++) {
+
+      //     if (task.languages[index2].id == this.filter.languageId) {
+      //       this.saveTask = task;
+      //       this.filteredTasks.push(this.saveTask);
+      //     }
+      //   }
+      // }
+  //   }
+  //   else if (this.filter.summaryKeyWord) {
+  //     this.onTaskSummaryFilter();
+  //     tasks = this.filteredTasks;
+  //   }
+  //   else {
+  //     this.filteredTasks = this.allTasks;
+  //   }
+  // }
+
+  // onTaskSummaryFilter() {
+
+  //   var tasks = this.allTasks;
+
+  //   if (this.filter.summaryKeyWord) {
+  //     this.filteredTasks = tasks.filter(
+  //       task => task.summary.toLocaleLowerCase().includes(this.filter.summaryKeyWord.toLocaleLowerCase())
+  //     );
+  //   }
+  //   else if (this.filter.languageId) {
+  //     this.onLanguageFilterChange();
+  //   }
+  //   else {
+  //     this.filteredTasks = this.allTasks;
+  //   }
+
+  //   // if (this.filter.languageId) {
+  //   //   this.onLanguageFilterChange();
+  //   //   tasks = this.filteredTasks;
+  //   // }
+  //   // else {
+  //   //   tasks = this.allTasks;
+  //   // }
+
+  //   // if (this.filter.summaryKeyWord) {
+  //   //   this.filteredTasks = tasks.filter(
+  //   //     task => task.summary.toLocaleLowerCase().includes(this.filter.summaryKeyWord.toLocaleLowerCase()));
+  //   // }
+  //   // else if (this.filter.languageId) {
+  //   //   this.onLanguageFilterChange();
+  //   //   tasks = this.filteredTasks;
+  //   // }
+  //   // else {
+  //   //   this.filteredTasks = this.allTasks;
+  //   // }
+
+  // }
   }
 }
--- Models/TaskQuery.cs
+++ Models/TaskQuery.cs
@@ -7,7 +7,7 @@ namespace ProgrammingLog.Models
     {
         public double? Hours { get; set; }
         public int? LanguageId { get; set; }
-        public string SummaryKeyWord { get; set; }
+        public string TaskSummary { get; set; }
         public string Description { get; set; }
         public string SortBy { get; set; }
         public bool IsSortAscending { get; set; }
